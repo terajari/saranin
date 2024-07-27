9603e7b6b5e1f148d1e1478de60877ade82551c3
agusanggarwanto <agusanggarwanto@gmail.com>
2024-07-27 14:46:39 +0700

    posts list

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -2,7 +2,7 @@
 import classes from './NewPost.module.css';
 import { useState } from 'react';
 
-function NewPost({onCancel}) {
+function NewPost({onCancel, onAddPost}) {
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
 
@@ -21,7 +21,7 @@ function NewPost({onCancel}) {
       body: enteredBody,
       author: enteredAuthor
     }
-    console.log(postData)
+    onAddPost(postData)
     onCancel()
   }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,19 +3,26 @@
 import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
+import { useState } from 'react';
+import Post from './Post'
 
 function PostsList({isClosing, onStopPosting}) {
+  const [posts, setPost] = useState([])
 
+  const addPost = (postData) => setPost([postData, ...posts])
   return (
     <>
     {isClosing && <Modal onClose={onStopPosting}>
         <NewPost
           onCancel={onStopPosting}
+          onAddPost={addPost}
         />
       </Modal>}
       
-      <ul className={classes.posts}>
-      </ul>
+      {posts.length > 0 ? <ul className={classes.posts}>
+        {posts.map((post) => <Post key={post.author} author={post.author} body={post.body}/>)}
+      </ul>: <div style={{textAlign: 'center', color: 'white'}}>Belum ada saran.
+        <p>Tambahkan saran baru!</p></div>}
     </>
   );
 }
